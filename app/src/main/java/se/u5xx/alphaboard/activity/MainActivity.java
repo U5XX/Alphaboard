717c7b1476bf988bd529c2e46710609742c8ac83
Simon Cedergren <simon@tuxflux.se>
2015-08-17 00:37:45 +0200

    * Updated butterknife to latest version
    * Added toolbar
    
    * Changed name of the app
    
    * Added style.xml for >lollipop

--- app/src/main/java/se/u5xx/alphaboard/activity/MainActivity.java
+++ app/src/main/java/se/u5xx/alphaboard/activity/MainActivity.java
@@ -2,17 +2,31 @@ package se.u5xx.alphaboard.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import se.u5xx.alphaboard.BuildConfig;
 import se.u5xx.alphaboard.R;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+    private Timber.DebugTree timberDebugTree = new Timber.DebugTree();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (BuildConfig.DEBUG)
+            Timber.plant(timberDebugTree); // Enable log on debug builds.
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        if(null != getSupportActionBar()) getSupportActionBar().setTitle(R.string.app_name);
     }
 
     @Override
